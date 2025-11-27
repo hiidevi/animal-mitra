27b3d1b41780e1fd25f979e1eaaa95d91eb0d065
Hiidevi <devipanchal0013@gmail.com>
2025-11-27 17:56:33 +0530

    Add build script for Render

---
+++ build.sh
@@ -0,0 +1,15 @@
+'EOF'
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Build complete!"
+EOF
